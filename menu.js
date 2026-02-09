b194cf6a9cdb292dd4ee908ce71b59b4d14bc23c
BYIRINGIRO Sam Franco <ancosamfranco250@gmail.com>
2026-02-09 16:00:43 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,5 +2,5 @@ const menuBtn = document.getElementById("menuBtn");
 const sidebar = document.getElementById("sidebar");
 
 menuBtn?.addEventListener("click", () => {
-  sidebar.classList.toggle("-translate-x-full");
+  sidebar.classList.toggle("-translate-x-full shadow-xl");
 });
